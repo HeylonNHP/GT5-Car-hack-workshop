650d27903ac24b86db969ff5e3990f93b5963d60
Heylon White <heylon96@hotmail.com>
2025-03-16 18:44:33 +1000

    `Refactor SaveGearRatioToGt5Save to improve input validation`
    Replaced try-catch block with a more robust decimal.TryParse method to validate input, avoiding unnecessary exceptions. Simplified the logic for calculating and saving gear ratios. Also removed unnecessary trailing whitespace in related method calls.

--- GT5 Car hack workshop 2.4/TransmissionEditor.cs
+++ GT5 Car hack workshop 2.4/TransmissionEditor.cs
@@ -216,21 +216,17 @@ namespace GT5_Car_hack_workshop_2
 
         private void SaveGearRatioToGt5Save(string textBoxValue, int moffOffset1, int moffOffset2)
         {
-            try
+            if (!decimal.TryParse(textBoxValue, out var parsedRatio))
             {
-                var working = (int)(decimal.Parse(textBoxValue) * 1000);
-                checked
-                {
-                    MyProject.Forms.Form1.Gt5Save[moffOffset1] =
-                        byte.Parse(working.ToString("X4").Substring(0, 2), NumberStyles.HexNumber);
-                    MyProject.Forms.Form1.Gt5Save[moffOffset2] =
-                        byte.Parse(working.ToString("X4").Substring(2, 2), NumberStyles.HexNumber);
-                }
+                return;
             }
-            catch (Exception ex)
-            {
-                // Handle the exception appropriately, e.g., log it or display an error message.
-            }
+
+            var intValue = (int)(parsedRatio * 1000);
+
+            MyProject.Forms.Form1.Gt5Save[moffOffset1] =
+                byte.Parse(intValue.ToString("X4").Substring(0, 2), NumberStyles.HexNumber);
+            MyProject.Forms.Form1.Gt5Save[moffOffset2] =
+                byte.Parse(intValue.ToString("X4").Substring(2, 2), NumberStyles.HexNumber);
         }
 
 
@@ -238,37 +234,37 @@ namespace GT5_Car_hack_workshop_2
         {
             // Gear 1
             SaveGearRatioToGt5Save(TextBox1.Text, MyProject.Forms.Form1.Moff - 73, MyProject.Forms.Form1.Moff - 72);
-            
+
             // Gear 2
             SaveGearRatioToGt5Save(TextBox2.Text, MyProject.Forms.Form1.Moff - 71, MyProject.Forms.Form1.Moff - 70);
-            
+
             // Gear 3
             SaveGearRatioToGt5Save(TextBox3.Text, MyProject.Forms.Form1.Moff - 69, MyProject.Forms.Form1.Moff - 68);
-            
+
             // Gear 4
             SaveGearRatioToGt5Save(TextBox4.Text, MyProject.Forms.Form1.Moff - 67, MyProject.Forms.Form1.Moff - 66);
-            
+
             // Gear 5
             SaveGearRatioToGt5Save(TextBox5.Text, MyProject.Forms.Form1.Moff - 65, MyProject.Forms.Form1.Moff - 64);
-            
+
             // Gear 6
             SaveGearRatioToGt5Save(TextBox6.Text, MyProject.Forms.Form1.Moff - 63, MyProject.Forms.Form1.Moff - 62);
-            
+
             // Gear 7
             SaveGearRatioToGt5Save(TextBox7.Text, MyProject.Forms.Form1.Moff - 61, MyProject.Forms.Form1.Moff - 60);
-            
+
             // Gear 8
             SaveGearRatioToGt5Save(TextBox8.Text, MyProject.Forms.Form1.Moff - 59, MyProject.Forms.Form1.Moff - 58);
-            
+
             // Gear 9
             SaveGearRatioToGt5Save(TextBox9.Text, MyProject.Forms.Form1.Moff - 57, MyProject.Forms.Form1.Moff - 56);
-            
+
             // Gear 10
             SaveGearRatioToGt5Save(TextBox10.Text, MyProject.Forms.Form1.Moff - 55, MyProject.Forms.Form1.Moff - 54);
-            
+
             // Gear 11
             SaveGearRatioToGt5Save(TextBox11.Text, MyProject.Forms.Form1.Moff - 53, MyProject.Forms.Form1.Moff - 52);
-            
+
             // Final drive
             SaveGearRatioToGt5Save(TextBox12.Text, MyProject.Forms.Form1.Moff - 51, MyProject.Forms.Form1.Moff - 50);
 
