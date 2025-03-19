df49a5418e0cc6629f6a34a003a1923760c7fc7a
Heylon White <heylon96@hotmail.com>
2025-03-20 00:05:37 +1000

    Add Customperformance resource and refine UI layout
    Integrated 'Customperformance.resx' as an embedded resource and updated associated designer file to adjust UI control dimensions, positioning, and styling. These updates improve usability and align the interface with the new resource implementation.

--- GT5 Car hack workshop 2.4/Customperformance.Designer.cs
+++ GT5 Car hack workshop 2.4/Customperformance.Designer.cs
@@ -34,6 +34,10 @@
 		}
 
 		// Token: 0x06000023 RID: 35 RVA: 0x00002D6C File Offset: 0x0000116C
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
 		private void InitializeComponent()
 		{
 			this.Label1 = new System.Windows.Forms.Label();
@@ -55,129 +59,204 @@
 			this.Label6 = new System.Windows.Forms.Label();
 			this.CheckBox1 = new System.Windows.Forms.CheckBox();
 			this.SuspendLayout();
+			// 
+			// Label1
+			// 
 			this.Label1.AutoSize = true;
 			this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
 			this.Label1.ForeColor = System.Drawing.Color.Red;
-			this.Label1.Location = new System.Drawing.Point(13, 13);
+			this.Label1.Location = new System.Drawing.Point(17, 16);
+			this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.Label1.Name = "Label1";
 			this.Label1.Size = new System.Drawing.Size(40, 13);
 			this.Label1.TabIndex = 0;
 			this.Label1.Text = "Turbo";
+			// 
+			// Label2
+			// 
 			this.Label2.AutoSize = true;
 			this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
 			this.Label2.ForeColor = System.Drawing.Color.Red;
-			this.Label2.Location = new System.Drawing.Point(7, 39);
+			this.Label2.Location = new System.Drawing.Point(9, 48);
+			this.Label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.Label2.Name = "Label2";
 			this.Label2.Size = new System.Drawing.Size(46, 13);
 			this.Label2.TabIndex = 1;
 			this.Label2.Text = "Engine";
+			// 
+			// Label3
+			// 
 			this.Label3.AutoSize = true;
 			this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
 			this.Label3.ForeColor = System.Drawing.Color.Red;
-			this.Label3.Location = new System.Drawing.Point(15, 63);
+			this.Label3.Location = new System.Drawing.Point(20, 78);
+			this.Label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.Label3.Name = "Label3";
 			this.Label3.Size = new System.Drawing.Size(38, 13);
 			this.Label3.TabIndex = 2;
 			this.Label3.Text = "Other";
-			this.TextBox1.Location = new System.Drawing.Point(59, 10);
+			// 
+			// TextBox1
+			// 
+			this.TextBox1.Location = new System.Drawing.Point(79, 12);
+			this.TextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.TextBox1.Name = "TextBox1";
-			this.TextBox1.Size = new System.Drawing.Size(100, 20);
+			this.TextBox1.Size = new System.Drawing.Size(132, 22);
 			this.TextBox1.TabIndex = 3;
 			this.TextBox1.Text = "5415";
-			this.TextBox2.Location = new System.Drawing.Point(59, 36);
+			// 
+			// TextBox2
+			// 
+			this.TextBox2.Location = new System.Drawing.Point(79, 44);
+			this.TextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.TextBox2.Name = "TextBox2";
-			this.TextBox2.Size = new System.Drawing.Size(100, 20);
+			this.TextBox2.Size = new System.Drawing.Size(132, 22);
 			this.TextBox2.TabIndex = 4;
 			this.TextBox2.Text = "3826";
-			this.TextBox3.Location = new System.Drawing.Point(59, 84);
+			// 
+			// TextBox3
+			// 
+			this.TextBox3.Location = new System.Drawing.Point(79, 103);
+			this.TextBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.TextBox3.Name = "TextBox3";
-			this.TextBox3.Size = new System.Drawing.Size(100, 20);
+			this.TextBox3.Size = new System.Drawing.Size(132, 22);
 			this.TextBox3.TabIndex = 5;
 			this.TextBox3.Text = "5180";
-			this.TextBox4.Location = new System.Drawing.Point(59, 110);
+			// 
+			// TextBox4
+			// 
+			this.TextBox4.Location = new System.Drawing.Point(79, 135);
+			this.TextBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.TextBox4.Name = "TextBox4";
-			this.TextBox4.Size = new System.Drawing.Size(100, 20);
+			this.TextBox4.Size = new System.Drawing.Size(132, 22);
 			this.TextBox4.TabIndex = 6;
 			this.TextBox4.Text = "1361";
-			this.TextBox5.Location = new System.Drawing.Point(59, 136);
+			// 
+			// TextBox5
+			// 
+			this.TextBox5.Location = new System.Drawing.Point(79, 167);
+			this.TextBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.TextBox5.Name = "TextBox5";
-			this.TextBox5.Size = new System.Drawing.Size(100, 20);
+			this.TextBox5.Size = new System.Drawing.Size(132, 22);
 			this.TextBox5.TabIndex = 7;
 			this.TextBox5.Text = "856";
+			// 
+			// Label4
+			// 
 			this.Label4.AutoSize = true;
-			this.Label4.Location = new System.Drawing.Point(59, 63);
+			this.Label4.Location = new System.Drawing.Point(79, 78);
+			this.Label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.Label4.Name = "Label4";
-			this.Label4.Size = new System.Drawing.Size(109, 13);
+			this.Label4.Size = new System.Drawing.Size(144, 16);
 			this.Label4.TabIndex = 8;
 			this.Label4.Text = "----------------------------------";
+			// 
+			// Button1
+			// 
 			this.Button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.Button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-			this.Button1.Location = new System.Drawing.Point(182, 274);
+			this.Button1.Location = new System.Drawing.Point(243, 337);
+			this.Button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Button1.Name = "Button1";
-			this.Button1.Size = new System.Drawing.Size(57, 23);
+			this.Button1.Size = new System.Drawing.Size(76, 28);
 			this.Button1.TabIndex = 9;
 			this.Button1.Text = "Apply";
 			this.Button1.UseVisualStyleBackColor = false;
 			this.Button1.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// Button2
+			// 
 			this.Button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.Button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-			this.Button2.Location = new System.Drawing.Point(121, 274);
+			this.Button2.Location = new System.Drawing.Point(161, 337);
+			this.Button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Button2.Name = "Button2";
-			this.Button2.Size = new System.Drawing.Size(55, 23);
+			this.Button2.Size = new System.Drawing.Size(73, 28);
 			this.Button2.TabIndex = 10;
 			this.Button2.Text = "Discard";
 			this.Button2.UseVisualStyleBackColor = false;
 			this.Button2.Click += new System.EventHandler(this.Button2_Click);
+			// 
+			// Button3
+			// 
 			this.Button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.Button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-			this.Button3.Location = new System.Drawing.Point(0, 263);
+			this.Button3.Location = new System.Drawing.Point(0, 324);
+			this.Button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Button3.Name = "Button3";
-			this.Button3.Size = new System.Drawing.Size(115, 34);
+			this.Button3.Size = new System.Drawing.Size(153, 42);
 			this.Button3.TabIndex = 11;
 			this.Button3.Text = "load current car performance";
 			this.Button3.UseVisualStyleBackColor = false;
 			this.Button3.Click += new System.EventHandler(this.Button3_Click);
+			// 
+			// Label5
+			// 
 			this.Label5.AutoSize = true;
 			this.Label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
 			this.Label5.ForeColor = System.Drawing.Color.Red;
-			this.Label5.Location = new System.Drawing.Point(7, 87);
+			this.Label5.Location = new System.Drawing.Point(9, 107);
+			this.Label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.Label5.Name = "Label5";
 			this.Label5.Size = new System.Drawing.Size(52, 13);
 			this.Label5.TabIndex = 12;
 			this.Label5.Text = "Exhaust";
-			this.TextBox6.Location = new System.Drawing.Point(59, 162);
+			// 
+			// TextBox6
+			// 
+			this.TextBox6.Location = new System.Drawing.Point(79, 199);
+			this.TextBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.TextBox6.Name = "TextBox6";
-			this.TextBox6.Size = new System.Drawing.Size(100, 20);
+			this.TextBox6.Size = new System.Drawing.Size(132, 22);
 			this.TextBox6.TabIndex = 13;
 			this.TextBox6.Text = "856";
-			this.TextBox7.Location = new System.Drawing.Point(59, 188);
+			// 
+			// TextBox7
+			// 
+			this.TextBox7.Location = new System.Drawing.Point(79, 231);
+			this.TextBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.TextBox7.Name = "TextBox7";
-			this.TextBox7.Size = new System.Drawing.Size(100, 20);
+			this.TextBox7.Size = new System.Drawing.Size(132, 22);
 			this.TextBox7.TabIndex = 14;
 			this.TextBox7.Text = "824";
-			this.TextBox8.Location = new System.Drawing.Point(59, 214);
+			// 
+			// TextBox8
+			// 
+			this.TextBox8.Location = new System.Drawing.Point(79, 263);
+			this.TextBox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.TextBox8.Name = "TextBox8";
-			this.TextBox8.Size = new System.Drawing.Size(100, 20);
+			this.TextBox8.Size = new System.Drawing.Size(132, 22);
 			this.TextBox8.TabIndex = 15;
 			this.TextBox8.Text = "829";
+			// 
+			// Label6
+			// 
 			this.Label6.AutoSize = true;
 			this.Label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline);
 			this.Label6.ForeColor = System.Drawing.Color.Red;
-			this.Label6.Location = new System.Drawing.Point(12, 237);
+			this.Label6.Location = new System.Drawing.Point(16, 292);
+			this.Label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.Label6.Name = "Label6";
 			this.Label6.Size = new System.Drawing.Size(212, 26);
 			this.Label6.TabIndex = 16;
 			this.Label6.Text = "The values in these boxes must not exceed\r\n65535";
+			// 
+			// CheckBox1
+			// 
 			this.CheckBox1.AutoSize = true;
-			this.CheckBox1.Location = new System.Drawing.Point(166, 13);
+			this.CheckBox1.Location = new System.Drawing.Point(221, 16);
+			this.CheckBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.CheckBox1.Name = "CheckBox1";
-			this.CheckBox1.Size = new System.Drawing.Size(121, 17);
+			this.CheckBox1.Size = new System.Drawing.Size(146, 20);
 			this.CheckBox1.TabIndex = 17;
 			this.CheckBox1.Text = "Don\'t install any part";
 			this.CheckBox1.UseVisualStyleBackColor = true;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			// 
+			// Customperformance
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(283, 299);
+			this.ClientSize = new System.Drawing.Size(377, 368);
 			this.Controls.Add(this.CheckBox1);
 			this.Controls.Add(this.Label6);
 			this.Controls.Add(this.TextBox8);
@@ -197,6 +276,7 @@
 			this.Controls.Add(this.Label2);
 			this.Controls.Add(this.Label1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "Customperformance";
