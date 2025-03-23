676dcb1e1d3081f16b8f2f3774d0baec4450f3d7
Heylon White <heylon96@hotmail.com>
2025-03-23 13:33:18 +1000

    Update AssemblyInfo to version 2.5.1.0 and copyright year to 2025

--- GT5 Car hack workshop/Properties/AssemblyInfo.cs
+++ GT5 Car hack workshop/Properties/AssemblyInfo.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyVersion("2.5.1.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: Guid("567be9e8-cd5e-4e2f-b572-03f8a94e0036")]
-[assembly: AssemblyCopyright("Copyright © NHP 2013")]
+[assembly: AssemblyCopyright("Copyright © NHP 2025")]
 [assembly: AssemblyProduct("GT5 Car hack workshop 2")]
 [assembly: AssemblyCompany("Nuclear Heylon Productions (HeylonNHP)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTitle("GT5 Car hack workshop 2")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.1.0")]
